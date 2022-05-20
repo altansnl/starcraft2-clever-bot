e743b8d5e90b4f266f8c916b4001e776a2381143
altansnl <altansenel@sabanciuniv.edu>
2022-05-20 02:47:57 +0200

    cheese build done until 20

--- stalker_cheese.py
+++ stalker_cheese.py
@@ -12,26 +12,75 @@ from greedy_bot import *
 class StalkerCheeseBot(BotAI):
     def __init__(self):
         self.nexus = None
+        self.chronoboost_count = 0
 
 
     async def on_step(self, iteration: int):
-
+        await self.distribute_workers()
+        
+        """
         if(self.nexus == None):
-            self.nexus = self.townhalls.ready.random 
+            self.nexus = self.townhalls.random 
+        """
+        self.nexus = self.townhalls.ready.random
 
-        if(self.supply_used < 14 and self.nexus.is_idle):
-            if self.can_afford(UnitTypeId.PROBE):
-                await self.build(UnitTypeId.PROBE, near=self.nexus.position.towards(self.game_info.map_center, 5))
-            return
-        elif(self.supply_used == 14):
-            builder_prob = self.units(UnitTypeId.PROBE).closest_to(self.nexus.position.towards(self.game_info.map_center, 5))
+        if(self.supply_used < 20 and self.nexus.is_idle):
+            if self.can_afford(UnitTypeId.PROBE) and self.already_pending(UnitTypeId.PROBE) == 0:
+                self.nexus.train(UnitTypeId.PROBE)
+
+        if(self.already_pending(UnitTypeId.GATEWAY) and self.chronoboost_count == 0):
+            if not self.nexus.has_buff(BuffId.CHRONOBOOSTENERGYCOST):
+                if self.nexus.energy >= 50:
+                    self.nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, self.nexus)
+                    print("Nexus is chrono boosted now!, -50 energy")
+                    self.chronoboost_count += 1
+
+        if(self.supply_used == 14):
+            placement = self.nexus.position.towards(self.game_info.map_center, 5)
+            builder_prob = self.workers.closest_to(placement)
             if(self.already_pending(UnitTypeId.PYLON) == 0 and self.can_afford(UnitTypeId.PYLON)):
-                await builder_prob.build(UnitTypeId.PYLON, near=self.nexus)
+                builder_prob.build(UnitTypeId.PYLON, placement)
             return
+        elif(self.supply_used == 16):
+            placement = self.structures(UnitTypeId.PYLON).random.position.towards(self.game_info.map_center, 4)
+            builder_prob = self.workers.closest_to(placement)
+            if(self.already_pending(UnitTypeId.GATEWAY) == 0 and self.can_afford(UnitTypeId.GATEWAY)):
+                builder_prob.build(UnitTypeId.GATEWAY, placement)
+        elif(self.supply_used == 17):
+            vgs = self.vespene_geyser.closer_than(15, self.nexus)
+            for vg in vgs:
+                if not self.can_afford(UnitTypeId.ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(vg.position)
+                if worker is None:
+                    break
+                if not self.gas_buildings or not self.gas_buildings.closer_than(1, vg):
+                    worker.build_gas(vg)
+                    worker.stop(queue=True)
+                    break
+        elif(self.supply_used == 18):
+            vgs = self.vespene_geyser.closer_than(15, self.nexus)
+            for vg in vgs:
+                if not self.can_afford(UnitTypeId.ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(vg.position)
+                if worker is None:
+                    break
+                if not self.gas_buildings or not self.gas_buildings.closer_than(1, vg):
+                    worker.build_gas(vg)
+                    worker.stop(queue=True)
+                    break
+        elif(self.supply_used == 20):
+            gateway_count = self.already_pending(UnitTypeId.GATEWAY) + self.structures(UnitTypeId.GATEWAY).ready.amount
+            if(gateway_count == 1 and self.can_afford(UnitTypeId.GATEWAY)):
+                pos = self.structures(UnitTypeId.GATEWAY).random.position
+                placement_position = await self.find_placement(UnitTypeId.GATEWAY, near=pos, placement_step=1)
+                builder_prob = self.workers.closest_to(placement_position)
+                builder_prob.build(UnitTypeId.GATEWAY, placement_position)
 
 
 
 run_game(maps.get("BlackburnAIE"), [
     Bot(Race.Protoss, StalkerCheeseBot(), name="Cheeser"),
     Computer(Race.Protoss, Difficulty.Easy)
-    ], realtime=True)
+    ], realtime=False)
